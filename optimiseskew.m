344b51c2c69d3357055a9bbb924ecafec13c47ec
edward-n-ward <31654574+edward-n-ward@users.noreply.github.com>
2018-08-24 17:49:44 +0100

    Delete optimiseskew.m

--- optimiseskew.m
+++
@@ -1,7 +0,0 @@
-function [pattern, k] = optimiseskew(pattern,image)
-f = @(k,pattern,image)immse(lensdistort(pattern,k),image);
-fun = @(k)f(k,pattern,image);
-estimate = 0;
-k = fminsearch(fun,estimate);
-pattern = lensdistort(pattern,k);
-end
